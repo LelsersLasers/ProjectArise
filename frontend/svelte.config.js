d37fcfb11459268ce3ddd49ea55141574a0c1411
Lord-Lelsers <millan.kumar.24@bishops.com>
2024-06-13 11:04:41 -0700

    Vercel adpater

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,13 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+// 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+// 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+// 		adapter: adapter()
+// 	}
+// };
+
+// export default config;
+
+import adapter from '@sveltejs/adapter-vercel';
+
+export default {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// see below for options that can be set here
+		})
 	}
 };
-
-export default config;
