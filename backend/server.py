2bf377d9c8dddf30fdb6da25961220d8be47cf32
Lord-Lelsers <millan.kumar.24@bishops.com>
2024-06-14 22:46:57 -0700

    full model path

--- backend/server.py
+++ backend/server.py
@@ -12,7 +12,7 @@ import cv2
 
 
 PORT = 3001
-MODEL_LOAD_PATH = "arise_fullsave_2-9"
+MODEL_LOAD_PATH = "/home/projectarise/ProjectArise/backend/arise_fullsave_2-9"
 REMOVE_JSON_PATH = "remove.json"
 INPUT_SIZE = 256
 LABELS = [
