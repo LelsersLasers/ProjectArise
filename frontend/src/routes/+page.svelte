9939c6aa2eec2b9b9193affbcc2032a33cfd3e2b
Lord-Lelsers <millan.kumar.24@bishops.com>
2024-06-24 19:50:42 -0400

    Typo

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,7 +3,7 @@
 
     const FLASK_URL = 'https://projectarise.pythonanywhere.com/';
     // const FLASK_URL = 'http://64.98.192.13:3001/';
-    const FLASK_URL = 'http://localhost:3001/';
+    // const FLASK_URL = 'http://localhost:3001/';
 
     let title = 'Project ARISE';
 
